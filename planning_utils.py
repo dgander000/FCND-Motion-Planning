aae8d22503673412c66a76e3124b28fde10d280e
Dan Gander <dgander000@gmail.com>
2021-01-23 10:43:24 -0600

    Added diagonal to A* and implemented collinearity path pruning

--- planning_utils.py
+++ planning_utils.py
@@ -55,6 +55,10 @@ class Action(Enum):
     EAST = (0, 1, 1)
     NORTH = (-1, 0, 1)
     SOUTH = (1, 0, 1)
+    NORTH_EAST = (-1, 1, np.sqrt(2))
+    SOUTH_EAST = (1, 1, np.sqrt(2))
+    NORTH_WEST = (-1, -1, np.sqrt(2))
+    SOUTH_WEST = (1, -1, np.sqrt(2))
 
     @property
     def cost(self):
@@ -84,6 +88,15 @@ def valid_actions(grid, current_node):
         valid_actions.remove(Action.WEST)
     if y + 1 > m or grid[x, y + 1] == 1:
         valid_actions.remove(Action.EAST)
+
+    if x - 1 < 0 or y + 1 > m or grid[x - 1, y + 1] == 1:
+        valid_actions.remove(Action.NORTH_EAST)
+    if x - 1 < 0 or y - 1 < 0 or grid[x - 1, y - 1] == 1:
+        valid_actions.remove(Action.NORTH_WEST)
+    if x + 1 > n or y + 1 > m or grid[x + 1, y + 1] == 1:
+        valid_actions.remove(Action.SOUTH_EAST)
+    if x + 1 > n or y - 1 < 0 or grid[x + 1, y - 1] == 1:
+        valid_actions.remove(Action.SOUTH_WEST)
 
     return valid_actions
 
@@ -144,3 +157,25 @@ def a_star(grid, h, start, goal):
 def heuristic(position, goal_position):
     return np.linalg.norm(np.array(position) - np.array(goal_position))
 
+def point(p):
+    return np.array([p[0], p[1], 1.]).reshape(1, -1)
+
+def collinearity_prune(path, epsilon=1e-5):
+    pruned_path = [i for i in path]
+
+    i = 0
+    while i < len(pruned_path) - 2:
+        p1 = point(pruned_path[i])
+        p2 = point(pruned_path[i+1])
+        p3 = point(pruned_path[i+2])
+
+        mat = np.vstack((p1, p2, p3))
+        if(abs(np.linalg.det(mat)) < epsilon):
+            pruned_path.remove(pruned_path[i+1]) 
+        else:
+            i += 1
+
+    return pruned_path
+
+def prune_path(path):
+    return collinearity_prune(path)
